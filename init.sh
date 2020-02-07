996436f773454826bf69660e05728252c9a4d844
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-02-07 14:06:18 +0300

    populate

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if ( which dirname ) && ( which realpath ); then
+  PROJECT_ROOT=$(dirname $(realpath $0))
+  cd "${PROJECT_ROOT}"
+fi
+
+git submodule update --init --recursive
+client/install.py
+
