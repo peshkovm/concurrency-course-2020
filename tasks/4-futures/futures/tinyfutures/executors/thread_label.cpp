5a9de843b7fa884769292a8c43c569c4908ceb8b
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-05-09 07:32:32 +0000

    FaultyFiber

--- tasks/4-futures/futures/tinyfutures/executors/thread_label.cpp
+++ tasks/4-futures/futures/tinyfutures/executors/thread_label.cpp
@@ -2,22 +2,25 @@
 
 #include <tinysupport/assert.hpp>
 
+#include <twist/strand/thread_local.hpp>
+
 namespace tiny::executors {
 
-static thread_local ThreadLabel thread_label;
+// Fibers execution backend support: thread_local -> ThreadLocal
+static twist::strand::ThreadLocal<std::string> thread_label;
 
 void LabelThread(const ThreadLabel& label) {
-  thread_label = label;
+  *thread_label = label;
 }
 
 void ExpectThread(const ThreadLabel& label) {
-  TINY_VERIFY(label == thread_label, "Unexpected thread label: '"
-                                         << thread_label << "', expected `"
-                                         << label << "`");
+  TINY_VERIFY(label == *thread_label,
+              "Unexpected thread label: '" << *thread_label
+                                           << "', expected `" << label << "`");
 }
 
 ThreadLabel GetThreadLabel() {
-  return thread_label;
+  return *thread_label;
 }
 
 }  // namespace tiny::executors
