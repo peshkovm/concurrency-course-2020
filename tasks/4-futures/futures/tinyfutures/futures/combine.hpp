9349c66e548b4cc0730943627dd96d7230c91889
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-05-11 19:44:37 +0000

    better combine template

--- tasks/4-futures/futures/tinyfutures/futures/combine.hpp
+++ tasks/4-futures/futures/tinyfutures/futures/combine.hpp
@@ -8,22 +8,28 @@ namespace tiny::futures {
 
 namespace detail {
 
-// Your combinators
+// Your combinators:
+// AllCombinator, FirstOfCombinator
+
+template<template <typename> class Combinator, typename T>
+auto Combine(std::vector<Future<T>>& futures) {
+  return;  // Not implemented
+}
 
 }  // namespace detail
 
 // All values / first error
 template <typename T>
 Future<std::vector<T>> All(std::vector<Future<T>> futures) {
-  // Not implemented
-  return MakeBrokenFuture<std::vector<T>>();
+  return MakeBrokenFuture<std::vector<T>>();  // Not implemented
+  // Use detail::Combine
 }
 
 // First value or last error
 template <typename T>
 Future<T> FirstOf(std::vector<Future<T>> futures) {
-  // Not implemented
-  return MakeBrokenFuture<T>();
+  return MakeBrokenFuture<T>();  // Not implemented
+  // Use detail::Combine
 }
 
 }  // namespace tiny::futures
