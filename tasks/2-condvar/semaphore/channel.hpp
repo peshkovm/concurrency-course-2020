54749a750ff14470a94f8bf8c9f3098803e92a22
Maksim Peshkov <maxpeshkov5301@gmail.com>
2020-07-07 21:11:28 +0300

    Done
    Code is not working due to some magic with pointers and copy constructors. But algorithm seems correct.

--- tasks/2-condvar/semaphore/channel.hpp
+++ tasks/2-condvar/semaphore/channel.hpp
@@ -3,26 +3,44 @@
 #include "semaphore.hpp"
 
 #include <deque>
+#include <iostream>
 
 namespace solutions {
 
 template <typename T>
 class BufferedChannel {
  public:
-  explicit BufferedChannel(size_t /*capacity*/) {
+  explicit BufferedChannel(size_t capacity) : queue_is_full_(capacity) {
     // Not implemented
   }
 
-  void Send(T /*item*/) {
-    // Not implemented
+  void Send(T item) {
+    queue_is_full_.Acquire();
+    mutex_.Acquire();
+    deque_.emplace_back(&item);
+    queue_is_empty_.Release();
+    mutex_.Release();
   }
 
   T Receive() {
     // Not implemented
+    queue_is_empty_.Acquire();
+    mutex_.Acquire();
+    T* item_ptr = deque_.back();
+    deque_.pop_back();
+    queue_is_full_.Release();
+    mutex_.Release();
+
+    T item = *item_ptr;
+    return item;
   }
 
  private:
   // Use only semaphores for synchronization
+  solutions::Semaphore queue_is_full_;
+  solutions::Semaphore queue_is_empty_{0};
+  solutions::Semaphore mutex_{1};
+  std::deque<T*> deque_;
 };
 
 }  // namespace solutions
--- tasks/2-condvar/semaphore/semaphore.hpp
+++ tasks/2-condvar/semaphore/semaphore.hpp
@@ -7,20 +7,32 @@ namespace solutions {
 
 class Semaphore {
  public:
-  explicit Semaphore(size_t /*initial_count*/) {
+  explicit Semaphore(size_t initial_count) {
     // Not implemented
+    count_of_tickets_ = initial_count;
   }
 
   void Acquire() {
     // Not implemented
+    std::unique_lock<twist::stdlike::mutex> lock(mutex_);
+    while (count_of_tickets_ == 0) {
+      semaphore_not_empty_.wait(lock);
+    }
+    count_of_tickets_--;
   }
 
   void Release() {
     // Not implemented
+    std::unique_lock<twist::stdlike::mutex> lock(mutex_);
+    count_of_tickets_++;
+    semaphore_not_empty_.notify_one();
   }
 
  private:
   // Your code goes here
+  twist::stdlike::mutex mutex_;
+  twist::stdlike::condition_variable semaphore_not_empty_;
+  size_t count_of_tickets_;
 };
 
 }  // namespace solutions
