6514903261c7a279619de2601fd297d1257d8bc7
Maksim Peshkov <maxpeshkov5301@gmail.com>
2021-02-23 16:09:28 +0300

    Done

--- tasks/3-tinyfiber/coroutine/fiber.cpp
+++ tasks/3-tinyfiber/coroutine/fiber.cpp
@@ -6,14 +6,24 @@ namespace tinyfiber {
 
 void Spawn(FiberRoutine routine, ThreadPool& thread_pool) {
   // Not implemented
+  thread_pool.Submit([&]() {
+    auto coro = new coroutine::Coroutine(routine);
+    coro->Resume();
+  });
 }
 
 void Spawn(FiberRoutine routine) {
   // Not implemented
+  auto coro = new coroutine::Coroutine(routine);
+  coro->Resume();
 }
 
 void Yield() {
   // Not implemented
+  auto coro = coroutine::GetCurrentCoroutine();
+
+  ThreadPool::Current()->SubmitContinuation([&]() { coro->Resume(); });
+  coro->Suspend();
 }
 
 }  // namespace tinyfiber
